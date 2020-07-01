014d1f65f45d30af1a6b6ed06efbf095ddcd88f0
Vijay Thapa <vijaythapa333@gmail.com>
2020-07-01 11:50:59 +0545

    Apply Leave and Send Feedback by Students Completed

--- student_management_app/StudentViews.py
+++ student_management_app/StudentViews.py
@@ -5,7 +5,7 @@ from django.core.files.storage import FileSystemStorage #To upload Profile Pictu
 from django.urls import reverse
 import datetime # To Parse input DateTime into Python Date Time Object
 
-from student_management_app.models import CustomUser, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport
+from student_management_app.models import CustomUser, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, LeaveReportStudent, FeedBackStudent
 
 
 def student_home(request):
@@ -63,8 +63,63 @@ def student_view_attendance_post(request):
         }
 
         return render(request, 'student_template/student_attendance_data.html', context)
-        # return HttpResponse("OK")
-    # return redirect('student_view_attendance')
+       
+
+def student_apply_leave(request):
+    student_obj = Students.objects.get(admin=request.user.id)
+    leave_data = LeaveReportStudent.objects.filter(student_id=student_obj)
+    context = {
+        "leave_data": leave_data
+    }
+    return render(request, 'student_template/student_apply_leave.html', context)
+
+
+def student_apply_leave_save(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method")
+        return redirect('student_apply_leave')
+    else:
+        leave_date = request.POST.get('leave_date')
+        leave_message = request.POST.get('leave_message')
+
+        student_obj = Students.objects.get(admin=request.user.id)
+        try:
+            leave_report = LeaveReportStudent(student_id=student_obj, leave_date=leave_date, leave_message=leave_message, leave_status=0)
+            leave_report.save()
+            messages.success(request, "Applied for Leave.")
+            return redirect('student_apply_leave')
+        except:
+            messages.error(request, "Failed to Apply Leave")
+            return redirect('student_apply_leave')
+
+
+def student_feedback(request):
+    student_obj = Students.objects.get(admin=request.user.id)
+    feedback_data = FeedBackStudent.objects.filter(student_id=student_obj)
+    context = {
+        "feedback_data": feedback_data
+    }
+    return render(request, 'student_template/student_feedback.html', context)
+
+
+def student_feedback_save(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method.")
+        return redirect('student_feedback')
+    else:
+        feedback = request.POST.get('feedback_message')
+        student_obj = Students.objects.get(admin=request.user.id)
+
+        try:
+            add_feedback = FeedBackStudent(student_id=student_obj, feedback=feedback, feedback_reply="")
+            add_feedback.save()
+            messages.success(request, "Feedback Sent.")
+            return redirect('student_feedback')
+        except:
+            messages.error(request, "Failed to Send Feedback.")
+            return redirect('student_feedback')
+
+
 
 
 
--- student_management_app/urls.py
+++ student_management_app/urls.py
@@ -55,4 +55,8 @@ urlpatterns = [
     path('student_home/', StudentViews.student_home, name="student_home"),
     path('student_view_attendance/', StudentViews.student_view_attendance, name="student_view_attendance"),
     path('student_view_attendance_post/', StudentViews.student_view_attendance_post, name="student_view_attendance_post"),
+    path('student_apply_leave/', StudentViews.student_apply_leave, name="student_apply_leave"),
+    path('student_apply_leave_save/', StudentViews.student_apply_leave_save, name="student_apply_leave_save"),
+    path('student_feedback/', StudentViews.student_feedback, name="student_feedback"),
+    path('student_feedback_save/', StudentViews.student_feedback_save, name="student_feedback_save"),
 ]
