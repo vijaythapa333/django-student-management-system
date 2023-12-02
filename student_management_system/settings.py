2e2cb82ae24b532f0f68fb21099b39aca0ecb848
Anthony Ogolla <71929882+Antoniomcogolla019@users.noreply.github.com>
2023-12-02 13:26:52 +0300

    Update settings.py

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '(i#*06f#keydy_fh17bf=$0f6v)^wr^l7*u4gq42m*sztu#2_m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
